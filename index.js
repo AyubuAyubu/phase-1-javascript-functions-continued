1c1db0b8d552347e6ea13f6672e976623e8f98b1
AyubuAyubu <ayubu6143@gmail.com>
2022-08-22 20:50:34 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(defaultActivity = 'roller-skate') {
+    return `This Saturday, I want to ${defaultActivity}!`
+  }
+  
+  let mondayWork = function (defaultActivity = 'go to the office') {
+    return `This Monday, I will ${defaultActivity}.`
+  }
+  
+  let wrapAdjective = function (visualFlairString = '*') {
+    return function (adjective = 'special') {
+      return `You are ${visualFlairString}${adjective}${visualFlairString}!`
+    }
+  }
+  
+ 
+  
+  
